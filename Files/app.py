f1758f0ea30a5c34041a3143efeddf0956b0792b
qjlxg <qjlxg@163.com>
2024-10-21 20:30:41 +0800

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -179,7 +179,7 @@ def main():
         lines = f.readlines()
 
     num_lines = len(lines)
-    max_lines_per_file = 3600
+    max_lines_per_file = 7600
     num_files = (num_lines + max_lines_per_file - 1) // max_lines_per_file
 
     for i in range(num_files):
